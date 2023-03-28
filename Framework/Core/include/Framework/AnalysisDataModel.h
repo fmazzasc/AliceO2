5ccaf2fc5fdb0ed3804fc14b8b5f5fbdcbeb46fd
fmazzasc <fmazzasc@cern.ch>
2023-03-28 08:47:40 +0200

    Fix suffix in the track index

--- Framework/Core/include/Framework/AnalysisDataModel.h
+++ Framework/Core/include/Framework/AnalysisDataModel.h
@@ -863,11 +863,11 @@ using Decay3Body = Decay3Bodys::iterator;
 
 namespace strangenesstracking
 {
-DECLARE_SOA_INDEX_COLUMN(Track, track);                             //! Strange track index
-DECLARE_SOA_INDEX_COLUMN_FULL(ITSTrack, itsTrack, int, Tracks, ""); //! ITS index
-DECLARE_SOA_INDEX_COLUMN(Cascade, cascade);                         //! Cascade index
-DECLARE_SOA_INDEX_COLUMN(V0, v0);                                   //! V0 index
-DECLARE_SOA_INDEX_COLUMN(Decay3Body, decay3Body);                   //! Decay 3 body index
+DECLARE_SOA_INDEX_COLUMN(Track, track);                                 //! Strange track index
+DECLARE_SOA_INDEX_COLUMN_FULL(ITSTrack, itsTrack, int, Tracks, "_ITS"); //! ITS index
+DECLARE_SOA_INDEX_COLUMN(Cascade, cascade);                             //! Cascade index
+DECLARE_SOA_INDEX_COLUMN(V0, v0);                                       //! V0 index
+DECLARE_SOA_INDEX_COLUMN(Decay3Body, decay3Body);                       //! Decay 3 body index
 
 DECLARE_SOA_COLUMN(DecayX, decayX, float);             //! X coordinate of decay vertex
 DECLARE_SOA_COLUMN(DecayY, decayY, float);             //! Y coordinate of decay vertex
